7979619ad614a0229732536b078954e0535de525
180tech-lab <olanrewajue045@gmail.com>
2025-09-30 05:30:36 -0700

    added lorem 10 to book branch

---
+++ src/Background.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Background = () => {
+  return (
+    <div>Background Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, perspiciatis.</div>
+  )
+}
+
+export default Background
